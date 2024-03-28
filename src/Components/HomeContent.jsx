010123a3d92fd4e69ee40e36cc9fbc778476dda1
Lukmanam <lukkuptrklm2000@gmail.com>
2024-03-28 12:23:23 +0530

    ui change

--- src/Components/HomeContent.jsx
+++ src/Components/HomeContent.jsx
@@ -4,16 +4,19 @@ import ReactPlayer from 'react-player'
 
 const HomeContent = () => {
   return (
-    <div className='h-full bg-teal-100'>
+    <div className='h-full'>
   <div className="md:w-full lg:w-screen">
-                    <ReactPlayer
+                    {/* <ReactPlayer
                       url='../src/assets/homevideo.mp4'
-                      width="100%"
-                     height="80%"
+                      width="50%"
+                     height="50%"
                       loop={true}
                       playing={true}
                       controls={true}
-                    />
+                    /> */}
+                      <video autoPlay muted loop className='w-5/5 pl-6 pr-6'>
+        <source src="../src/assets/homevideo.mp4" />
+      </video>
                   </div>
 
     </div>
